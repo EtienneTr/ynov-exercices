dfa8b3c428e1a6bdd354b7ab685f71c09097325b
EtienneT <tienou42@gmail.com>
2017-11-30 17:22:19 +0100

    Routing exercice 2

---
+++ symfony/YnovSymfony/src/AppBundle/Controller/StudentsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use AppBundle\AppBundle;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class StudentsController extends Controller
+{
+    /**
+     * @Route("/students/create")
+     */
+    public function createStudentsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:students:index.html.twig');
+    }
+
+    /**
+     * @Route("/students/{number}")
+     * @Method({"GET","HEAD"})
+     */
+    public function getStudentsAction(Request $request, $number)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:students:details.html.twig', [
+            'number' => $number
+        ]);
+    }
+}
