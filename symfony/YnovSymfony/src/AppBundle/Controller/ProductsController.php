607930333f5573f010e8df9ec796d57ae0439144
EtienneT <tienou42@gmail.com>
2017-12-07 09:31:02 +0100

    Save product json obj

--- symfony/YnovSymfony/src/AppBundle/Controller/ProductsController.php
+++ symfony/YnovSymfony/src/AppBundle/Controller/ProductsController.php
@@ -2,28 +2,60 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 
 class ProductsController extends Controller
 {
     /**
-     * @Route("/display")
+     * @Route("/products/display")
      */
     public function DisplayAction()
     {
+        $repository = $this->get('doctrine')->getRepository('AppBundle:Product');
+        $products = $repository->findAll();
+
         return $this->render('AppBundle:Products:display.html.twig', array(
-            // ...
+            'products' => $products
         ));
     }
 
     /**
-     * @Route("/create")
+     * @Route("/products/create")
+     * @Method({"POST", "GET"})
      */
-    public function CreateAction()
+    public function CreateAction(Request $request)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $data = $request->getContent();
+
+        if (!empty($data))
+        {
+            $productJSON = json_decode($data)->product;
+
+            $product = new Product();
+            $product->setName($productJSON->name);
+            $product->setDescription($productJSON->description);
+            $product->setPrice(floatval($productJSON->price));
+            $product->setImageURI($productJSON->imageURI);
+
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+        }
+
+
+
+
+
         return $this->render('AppBundle:Products:create.html.twig', array(
-            // ...
+            "product" => $productJSON
         ));
     }
 
